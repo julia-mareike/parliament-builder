db8d90791d34ca74a9781e837f2f72de9740b6a2
Julia <jnmareike@gmail.com>
2018-05-30 21:44:47 +1200

    refactor functions slightly, fix small overhang issues

--- client/utils.js
+++ client/utils.js
@@ -1,8 +1,7 @@
 import {sum, values, findIndex, forEach, max as _max, indexOf} from 'lodash'
 
 export function formula (votes, idx) {
-  const result = votes / (idx * 2 + 1)
-  return result
+  return votes / (idx * 2 + 1)
 }
 
 export function adjustVotes (list) {
@@ -18,15 +17,16 @@ export function adjustVotes (list) {
 export function createVoteObject (obj) {
   const array = []
   forEach(obj, (votes, party) => {
-    if (votes > 0) {
-      const newObject = {}
-      newObject.party = party
-      newObject.votes = votes
-      newObject.adjusted = votes
-      newObject.allocated = 0
-      newObject.overhang = false
+    // if (votes > 0) {
+      const newObject = {
+        party: party,
+        votes: votes,
+        adjusted: votes,
+        allocated: 0,
+        overhang: false
+      }
       array.push(newObject)
-    }
+    // }
   })
   return array
 }
@@ -35,34 +35,32 @@ export function calculateVotes (electorates, votes) {
   const overhang = []
   const rawVotes = Object.assign({}, votes)
   for (let party in rawVotes) {
-    rawVotes[party] < 5 
-    ? (!electorates[party] 
-      ? rawVotes[party] = 0 
-      : overhang.push([party, electorates[party]])) 
-    : console.log('ok', party)
+    if (rawVotes[party] < 5) {
+      !electorates[party] 
+        ? rawVotes[party] = 0 
+        : overhang.push([party, electorates[party]]) 
+    }
   }
   const proportional = adjustVotes(rawVotes)
   let allVotes = createVoteObject(proportional)
   for (let party of overhang) {
     const target = findIndex(allVotes, ['party', party[0]])
-    if (allVotes[target]) {
     allVotes[target].allocated = 1
     allVotes[target].overhang = true
   }
-}
   return allVotes
 }
 
 export function saintLague (totals, idx = 0, seats = 120) {
   if (seats > 0) {
-    let array = []
-    for (let party of totals) {
-      const quotient = formula(party.adjusted, idx)
-      array.push(quotient)
-    }
-    const max = _max(array)
-    const current = indexOf(array, max)
-    if (!totals[current].overhang) totals[current].allocated++
+    const array = totals.map(party => {
+      if (!party.overhang)
+      return formula(party.adjusted, idx)
+    })
+    const current = indexOf(array, _max(array))
+    !totals[current].overhang
+      ? totals[current].allocated++
+      : seats++
     totals[current].adjusted = formula(totals[current].votes, totals[current].allocated)
     saintLague(totals, idx++, --seats)
   }
